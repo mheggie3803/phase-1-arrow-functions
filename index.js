e9db2577728484e99b11df93dfc442fcab6f3e35
Malcolm Heggie <mheggie@DESKTOP-8IG0D17.localdomain>
2021-08-31 16:32:24 -0400

    commit tree ed21d359830431f072edfa6b06b816e3ecba31e2

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(num1 = 2000, num2 = 100) {
+    return num1 / num2;
+}
+
+const square = (num1) => {
+    return num1 * num1;
+}
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
